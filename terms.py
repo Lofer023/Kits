a638765794987d6f674667059d668aaf9f88b298
Bob <blabla@gmail.com>
2026-03-11 19:00:16 +0200

    add other2

---
+++ terms.py
@@ -0,0 +1,131 @@
+# string = ['rooter','film','program']
+# assert 'film2' in string, 'Not in string'
+# assert 'program' in string, 'Not in string'
+
+from datetime import datetime
+import asyncio
+
+async def function():
+    await asyncio.sleep(0)
+    return 1
+
+async def function2():
+    await asyncio.sleep(0)
+    return 2
+
+async def make():
+
+    current_datetime = datetime.now()
+    print(current_datetime)
+    batch = asyncio.gather(function(),function2())
+    func_complete, func2_complete = await batch
+    end_datetime = datetime.now()
+
+    print(end_datetime-current_datetime)
+    print(func_complete)
+    print(func2_complete)
+
+result = asyncio.run(make())
+print(result)
+
+print()
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def sayHi(self):
+    # Calls its method .getName()
+    print(f"Hi! My name is {self.getName()}.")
+
+  def getName(self):
+    # Accesses the name variable
+    return self.name
+
+bob = Person("Bob")
+bob.sayHi()
+
+
+def odd_numbers():
+  start = 3
+  for n in range(start,10):
+    if n % 2 == 1:
+      continue
+    print(n)
+
+odd_numbers()
+
+
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def func2():
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            say_after(1, 'hello'))
+
+        task2 = tg.create_task(
+            say_after(2, 'world'))
+
+        print(f"started at {time.strftime('%X')}")
+
+        await task1, task2
+
+    # The await is implicit when the context manager exits.
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(func2())
+
+print()
+
+import asyncio
+
+async def bas1(arg):
+    print(arg)
+    return arg
+
+async def bas1feed():
+    task = asyncio.create_task(arg)
+    await task
+
+arg = 42
+arg = bas1(arg)
+asyncio.run(bas1feed())
+
+print()
+
+import asyncio
+from asyncio import TaskGroup
+
+class TerminateTaskGroup(Exception):
+    """Exception raised to terminate a task group."""
+
+async def force_terminate_task_group():
+    """Used to force termination of a task group."""
+    raise TerminateTaskGroup()
+
+async def job(task_id, sleep_time):
+    print(f'Task {task_id}: start')
+    await asyncio.sleep(sleep_time)
+    print(f'Task {task_id}: done')
+
+async def func3():
+    try:
+        async with TaskGroup() as group:
+            # spawn some tasks
+            group.create_task(job(1, 0.5))
+            group.create_task(job(2, 1.5))
+            # sleep for 1 second
+            await asyncio.sleep(1)
+            # add an exception-raising task to force the group to terminate
+            group.create_task(force_terminate_task_group())
+    except* TerminateTaskGroup:
+        pass
+
+asyncio.run(func3())
+
+
